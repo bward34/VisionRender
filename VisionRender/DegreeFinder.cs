3f4aea77e30106e41d81a78a1d2726fbfe505a1d
bward34 <brandon.ward@utah.edu>
2018-09-15 12:12:15 -0600

    inital commit

---
+++ VisionRender/DegreeFinder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VisionRender
+{
+    class DegreeFinder
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter x cordinant:");
+            Int32.TryParse(Console.ReadLine(), out int x);
+            Console.WriteLine("Enter y cordinant:");
+            Int32.TryParse(Console.ReadLine(), out int y);
+
+            double degrees = Math.Atan2(y , x) * (180 / Math.PI);
+            Console.WriteLine($"Degrees is {degrees}");
+
+        }
+    }
+}
