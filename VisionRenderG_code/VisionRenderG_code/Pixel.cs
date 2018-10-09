e85823100d4574a8462bd9d2489af51f0c381538
Taylor Smith <TaySmith16@gmail.com>
2018-10-09 09:24:19 -0600

    Created initial software. Still need to draw larger images in order to create differing angles.

---
+++ VisionRenderG_code/VisionRenderG_code/Pixel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisionRenderG_code
+{
+    class Pixel
+    {
+        public string Z { get; set; }
+        public string angle { get; set; }
+        public string radius { get; set; }
+        public string redVal { get; set; }
+        public string greenVal { get; set; }
+        public string blueVal { get; set; }
+
+        //Pixel()
+        //{
+        //    Z = null;
+        //    angle = null;
+        //    radius = null;
+        //    redVal = null;
+        //    greenVal = null;
+        //    blueVal = null;
+        //}
+
+        public void addZdata(int zVal)
+        {
+            Z = Convert.ToString(zVal, 2);
+            Z = Z.PadLeft(8, '0');
+        }
+
+        public void addAngle(double X, double Y, int divisions)
+        {
+            double calculatedAngle = Math.Atan(Y / X) * (180 / Math.PI); //Find the angle in degrees
+
+            if(divisions == 100)
+            {
+                calculatedAngle = calculatedAngle / 3.6;
+            }
+            if(divisions == 200)
+            {
+                calculatedAngle = calculatedAngle / 1.8;
+            }
+
+            int roundedAngle = (int) Math.Round(calculatedAngle);
+            angle = Convert.ToString(roundedAngle, 2);
+            angle = angle.PadLeft(8, '0');
+        }
+
+        public void addRadius(double X, double Y)
+        {
+            double scaleFactor = 0.75;
+            double calculatedRadius = Math.Sqrt((X * X) + (Y * Y));
+
+            while(calculatedRadius > 32)
+            {
+                calculatedRadius = calculatedRadius * scaleFactor;
+            }
+
+            int roundedRadius = (int) Math.Round(calculatedRadius);
+            radius = Convert.ToString(roundedRadius, 2);
+            radius = radius.PadLeft(8, '0');
+        }
+
+        void addColor(int redVal, int blueVal, int greenVal)
+        {
+
+        }
+    }
+}
---
+++ VisionRenderG_code/VisionRenderG_code/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace VisionRenderG_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fileName = "C:\\Users\\TaySm\\Desktop\\BlenderObjects\\redBlueCube.gcode";
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(fileName);
+
+                //Read the first line of text
+                line = sr.ReadLine();
+                String z_axis = @"([Z][0-9]*\.)";
+                String x_y_axis = @"([X][0-9]*\.)";
+                bool isZUpdated = false;
+                int zLayer = 0;
+                int index = 0;
+                Dictionary<int,List<Pixel>> objectData = new Dictionary<int,List<Pixel>>();
+                List<Pixel> currData = new List<Pixel>();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    
+                    Pixel Data = new Pixel();
+                    Match z_Result = Regex.Match(line, z_axis);
+                    int currZ = zLayer;
+                    if (z_Result.Success) {
+                        
+                        zLayer++;
+                        Data.addZdata(zLayer);
+                        Console.WriteLine(zLayer.ToString());
+
+                    }
+
+                    if(currZ < zLayer)
+                    {
+                        objectData.Add(currZ, currData);
+                        currData = new List<Pixel>();
+                    }
+                    
+                    Match x_yResult = Regex.Match(line, x_y_axis);
+                    if (x_yResult.Success)
+                    {
+                        bool isXdata = false;
+                        bool isYdata = false;
+                        string Xdata = "";
+                        string Ydata = "";
+                        
+                        foreach(char c in line)
+                        {
+                            if(isXdata && c == ' ')
+                            {
+                                isXdata = false;
+                            }
+                            if(isYdata && c == ' ')
+                            {
+                                isYdata = false;
+                            }
+                            if(isXdata)
+                            {
+                                Xdata += c;
+                            }
+                            if(isYdata)
+                            {
+                                Ydata += c;
+                            }
+                            if (c == 'X')
+                            {
+                                isXdata = true;
+                            }
+                            if (c == 'Y')
+                            {
+                                isYdata = true;
+                            }
+                        }
+                        Console.WriteLine(Xdata + " " + Ydata);
+
+                        double X;
+                        Double.TryParse(Xdata, out X);
+                        double Y;
+                        Double.TryParse(Xdata, out Y);
+
+                        Data.addAngle(X, Y, 360);
+                        Data.addRadius(X, Y);
+
+                        Console.WriteLine("Angle: " + Data.angle + " Radius: " + Data.radius);
+                        
+
+                        currData.Add(Data);
+                    }
+
+                    
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
