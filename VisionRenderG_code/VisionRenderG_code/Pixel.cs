8939ba77324f57df08f235227bdd135fc852c655
Taylor Smith <TaySmith16@gmail.com>
2018-10-27 09:55:20 -0600

    Finished writing render software logic and now I need to write to a file.

--- VisionRenderG_code/VisionRenderG_code/Pixel.cs
+++ VisionRenderG_code/VisionRenderG_code/Pixel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace VisionRenderG_code
 {
     class Pixel
@@ -14,26 +15,23 @@ namespace VisionRenderG_code
         public string redVal { get; set; }
         public string greenVal { get; set; }
         public string blueVal { get; set; }
+        public string adjustedX { get; set; }
+        public string adjustedY { get; set; }
+        public string angleNonBinary { get; set; }
+        public string nonBinaryZ { get; set; }
+        public string nonBinaryRadius { get; set; }
 
-        //Pixel()
-        //{
-        //    Z = null;
-        //    angle = null;
-        //    radius = null;
-        //    redVal = null;
-        //    greenVal = null;
-        //    blueVal = null;
-        //}
 
         public void addZdata(int zVal)
         {
+            nonBinaryZ = Convert.ToString(zVal);
             Z = Convert.ToString(zVal, 2);
             Z = Z.PadLeft(8, '0');
         }
 
         public void addAngle(double X, double Y, int divisions)
         {
-            double calculatedAngle = Math.Atan(Y / X) * (180 / Math.PI); //Find the angle in degrees
+            double calculatedAngle = Math.Atan(Y/ X) * (180 / Math.PI); //Find the angle in degrees
 
             if(divisions == 100)
             {
@@ -45,13 +43,15 @@ namespace VisionRenderG_code
             }
 
             int roundedAngle = (int) Math.Round(calculatedAngle);
+            angleNonBinary = Convert.ToString(roundedAngle);
             angle = Convert.ToString(roundedAngle, 2);
+
             angle = angle.PadLeft(8, '0');
         }
 
         public void addRadius(double X, double Y)
         {
-            double scaleFactor = 0.75;
+            double scaleFactor = 0.8;
             double calculatedRadius = Math.Sqrt((X * X) + (Y * Y));
 
             while(calculatedRadius > 32)
@@ -60,6 +60,7 @@ namespace VisionRenderG_code
             }
 
             int roundedRadius = (int) Math.Round(calculatedRadius);
+            nonBinaryRadius = Convert.ToString(roundedRadius);
             radius = Convert.ToString(roundedRadius, 2);
             radius = radius.PadLeft(8, '0');
         }
@@ -68,5 +69,11 @@ namespace VisionRenderG_code
         {
 
         }
+    }
+
+    class Coord
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
     }
 }
--- VisionRenderG_code/VisionRenderG_code/Program.cs
+++ VisionRenderG_code/VisionRenderG_code/Program.cs
@@ -5,9 +5,9 @@ using System.Text.RegularExpressions;
 
 namespace VisionRenderG_code
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             String fileName = "C:\\Users\\TaySm\\Desktop\\BlenderObjects\\redBlueCube.gcode";
             String line;
@@ -15,61 +15,63 @@ namespace VisionRenderG_code
             {
                 //Pass the file path and file name to the StreamReader constructor
                 StreamReader sr = new StreamReader(fileName);
-
-                //Read the first line of text
-                line = sr.ReadLine();
+                Dictionary<int, List<Pixel>> objectData = new Dictionary<int, List<Pixel>>();
+                Dictionary<int, List<Coord>> coordinate_Data = new Dictionary<int, List<Coord>>();
+                List<Pixel> currData = new List<Pixel>();
+                List<Coord> line_data = new List<Coord>();
                 String z_axis = @"([Z][0-9]*\.)";
                 String x_y_axis = @"([X][0-9]*\.)";
-                bool isZUpdated = false;
+                //Read the first line of text
+                line = sr.ReadLine();
                 int zLayer = 0;
-                int index = 0;
-                Dictionary<int,List<Pixel>> objectData = new Dictionary<int,List<Pixel>>();
-                List<Pixel> currData = new List<Pixel>();
+                double X = 0;
+                double Y = 0;
+                double X2 = 0;
+                double Y2 = 0;
+                int coord_Count = 0;
 
                 //Continue to read until you reach end of file
                 while (line != null)
                 {
-                    
                     Pixel Data = new Pixel();
                     Match z_Result = Regex.Match(line, z_axis);
                     int currZ = zLayer;
-                    if (z_Result.Success) {
-                        
-                        zLayer++;
-                        Data.addZdata(zLayer);
-                        Console.WriteLine(zLayer.ToString());
-
-                    }
-
-                    if(currZ < zLayer)
+                    if (z_Result.Success)
                     {
-                        objectData.Add(currZ, currData);
-                        currData = new List<Pixel>();
+                        zLayer++;
+                        Console.WriteLine("zLayer: " + zLayer);
                     }
-                    
+
+                    if (currZ < zLayer && zLayer > 1)
+                    {
+                        coordinate_Data.Add(currZ - 1, line_data);
+                        line_data = new List<Coord>();
+                        coord_Count = 0;
+                    }
+
                     Match x_yResult = Regex.Match(line, x_y_axis);
-                    if (x_yResult.Success)
+                    if (x_yResult.Success && zLayer > 1)
                     {
                         bool isXdata = false;
                         bool isYdata = false;
                         string Xdata = "";
                         string Ydata = "";
-                        
-                        foreach(char c in line)
+
+                        foreach (char c in line)
                         {
-                            if(isXdata && c == ' ')
+                            if (isXdata && c == ' ')
                             {
                                 isXdata = false;
                             }
-                            if(isYdata && c == ' ')
+                            if (isYdata && c == ' ')
                             {
                                 isYdata = false;
                             }
-                            if(isXdata)
+                            if (isXdata)
                             {
                                 Xdata += c;
                             }
-                            if(isYdata)
+                            if (isYdata)
                             {
                                 Ydata += c;
                             }
@@ -82,30 +84,40 @@ namespace VisionRenderG_code
                                 isYdata = true;
                             }
                         }
+
                         Console.WriteLine(Xdata + " " + Ydata);
-
-                        double X;
-                        Double.TryParse(Xdata, out X);
-                        double Y;
-                        Double.TryParse(Xdata, out Y);
-
-                        Data.addAngle(X, Y, 360);
-                        Data.addRadius(X, Y);
-
-                        Console.WriteLine("Angle: " + Data.angle + " Radius: " + Data.radius);
-                        
-
-                        currData.Add(Data);
+                        if (coord_Count == 0)
+                        {
+                            //double x1, y1;
+                            Double.TryParse(Xdata, out X);
+                            Double.TryParse(Ydata, out Y);
+                            //X = (int) Math.Round(x1);
+                            //Y = (int) Math.Round(y1);
+                            coord_Count++;
+                        }
+                        else if (coord_Count == 1)
+                        {
+                            //double x2, y2;
+                            Double.TryParse(Xdata, out X2);
+                            Double.TryParse(Ydata, out Y2);
+                            //X2 = (int) Math.Round(x2);
+                            //Y2 = (int) Math.Round(y2);
+                            Console.WriteLine("X,Y: " + X + " " + Y + "\n");
+                            Console.WriteLine("X2,Y2: " + X2 + " " + Y2 + "\n");
+                            DrawLine(coordinate_Data, X, Y, X2, Y2, currZ - 1, line_data);
+                            X = X2;
+                            Y = Y2;
+                        }
                     }
-
-                    
                     //Read the next line
                     line = sr.ReadLine();
                 }
-
+                coordinate_Data.Add(zLayer - 1, line_data);
                 //close the file
                 sr.Close();
-                Console.ReadLine();
+
+                AddDataToDictionary(coordinate_Data, objectData);
+
             }
             catch (Exception e)
             {
@@ -115,6 +127,121 @@ namespace VisionRenderG_code
             {
                 Console.WriteLine("Executing finally block.");
             }
+        }
+
+        private static void AddDataToDictionary(Dictionary<int, List<Coord>> coordinate_Data, Dictionary<int, List<Pixel>> objectData)
+        {
+            foreach(int key in coordinate_Data.Keys)
+            {
+                if (key != 0)
+                {
+                    List<Pixel> currPixelsLayer = new List<Pixel>();
+                    Console.WriteLine("Z layer: " + key + "\n");
+                    foreach (Coord coordinate in coordinate_Data[key])
+                    {
+                        Pixel currPixel = new Pixel();
+                        double xData = coordinate.X;
+                        double yData = coordinate.Y;
+                        currPixel.addAngle(xData, yData, 360);
+                        currPixel.addRadius(xData, yData);
+                        currPixel.addZdata(key);
+                        Console.WriteLine("Angle: " + currPixel.angle + "\n");
+                        Console.WriteLine("Radius: " + currPixel.radius + "\n");
+                        currPixelsLayer.Add(currPixel);
+                    }
+                    objectData.Add(key, currPixelsLayer);
+                    Console.WriteLine("\n");
+                }
+            }
+        }
+
+        private static List<Coord> DrawLine(this Dictionary<int, List<Coord>> coordinates, double x0, double y0, double x1, double y1, int z, List<Coord> line_data)
+        {
+            int dx = Math.Abs((int)(x1 - x0)), sx = x0 < x1 ? 1 : -1;
+            int dy = Math.Abs((int)(y1 - y0)), sy = y0 < y1 ? 1 : -1;
+            int err = (dx > dy ? dx : -dy) / 2, e2;
+            for (; ; )
+            {
+                double prevX;
+                double prevY;
+                Console.WriteLine("line data: " + x0 + " " + y0 + "\n");
+                Coord curr_coord = new Coord();
+                curr_coord.X = x0;
+                curr_coord.Y = y0;
+                prevX = x0;
+                prevY = y0;
+                line_data.Add(curr_coord);
+                if (x0 == x1 && y0 == y1) break;
+                e2 = err;
+                if (e2 > -dx)
+                {
+                    if (sx == -1)
+                    {
+                        if ((x0 + sx) < x1)
+                        {
+                            Coord end_coord = new Coord();
+                            end_coord.X = x1;
+                            end_coord.Y = y1;
+                            line_data.Add(end_coord);
+                            break;
+                        }
+                    }
+                    if (sx == 1)
+                    {
+                        if ((x0 + sx) > x1)
+                        {
+                            Coord end_coord = new Coord();
+                            end_coord.X = x1;
+                            end_coord.Y = y1;
+                            line_data.Add(end_coord);
+                            break;
+                        }
+                    }
+
+                    err -= dy;
+                    x0 += sx;
+                }
+                if (e2 < dy)
+                {
+                    if (sy == 1)
+                    {
+                        if ((y0 + sy) > y1)
+                        {
+                            Coord end_coord = new Coord();
+                            end_coord.X = x1;
+                            end_coord.Y = y1;
+                            line_data.Add(end_coord);
+                            break;
+                        }
+                    }
+                    if (sy == -1)
+                    {
+                        if ((y0 + sy) < x1)
+                        {
+                            Coord end_coord = new Coord();
+                            end_coord.X = x1;
+                            end_coord.Y = y1;
+                            line_data.Add(end_coord);
+                            break;
+                        }
+                    }
+
+                    err += dx;
+                    y0 += sy;
+                }
+                x0 = Math.Truncate(x0 * 1000) / 1000;
+                y0 = Math.Truncate(y0 * 1000) / 1000;
+                if(prevX == x0 && prevY == y0)
+                {
+                    Coord end_coord = new Coord();
+                    end_coord.X = x1;
+                    end_coord.Y = y1;
+                    line_data.Add(end_coord);
+                    break;
+                }
+            }
+            
+            return line_data;
         }
     }
 }
